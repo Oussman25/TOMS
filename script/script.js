16812360b33106a739068f6cff1fe3a847f144e6
Oussman25 <Oussman25@icloud.com>
2023-12-05 10:48:46 +0100

    update

--- script/script.js
+++ script/script.js
@@ -1,5 +1,6 @@
 const buttons = document.querySelectorAll("button");
-const meteoDiv =document.querySelector('.infoMeteo');
+const meteoDiv = document.querySelector('.infoMeteo');
+let srcImgMeteo
 
 buttons.forEach(button => 
     {button.addEventListener("click", () => {
@@ -7,13 +8,27 @@ buttons.forEach(button =>
         .then((response) => response.json())
         .then((data) => {
         //   console.log(data);
+        urlImgMeteo(data.conditionsMeteo)
     meteoDiv.style.display = "flex"
     meteoDiv.innerHTML = `<h2>${data.nom}</h2>
                     <p>Temperature : ${data.temperature}°C</p>
                     <p>Conditions Météo: ${data.conditionsMeteo}</p>
+                    <img src=${srcImgMeteo} alt="Logo-Meteo">
                     <p>Humidité: ${data.humidite}%</p>
                     <p>Précipitations: ${data.precipitations}%</p>
                     <p>Indice UV ${data.indiceUV}</p>`
         });
     });
 });
+
+function urlImgMeteo(conditionsMeteo) {   
+    if (conditionsMeteo == "Pluvieux") {
+        srcImgMeteo = "../img/pluie.png"
+    }
+    if (conditionsMeteo == "Ensoleillé") {
+        srcImgMeteo = "../img/soleil.png"
+    }
+    if (conditionsMeteo == "Nuageux") {
+        srcImgMeteo = "../img/nuage.png"
+    }
+}
